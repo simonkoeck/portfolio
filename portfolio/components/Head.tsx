d881ee52cff09f3d3c2c9f303acf9d18126cf25a
Simon Köck <54739715+simonkoeck@users.noreply.github.com>
2022-08-16 13:17:57 +0200

    add favicon

--- portfolio/components/Head.tsx
+++ portfolio/components/Head.tsx
@@ -20,7 +20,28 @@ export default function CustomHead({ title, description }: Props) {
       <meta property="og:site_name" content={title} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="theme-color" content="#E879F9" />
+      <meta name="theme-color" content="#121212" />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
     </Head>
   );
 }
