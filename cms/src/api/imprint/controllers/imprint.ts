5665598af439a9de8b875c75a8ea2be8d58022a7
Simon Köck <54739715+simonkoeck@users.noreply.github.com>
2022-08-16 12:08:34 +0200

    add cms

---
+++ cms/src/api/imprint/controllers/imprint.ts
@@ -0,0 +1,7 @@
+/**
+ *  imprint controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::imprint.imprint');
---
+++ cms/src/api/imprint/services/imprint.ts
@@ -0,0 +1,7 @@
+/**
+ * imprint service.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::imprint.imprint');
