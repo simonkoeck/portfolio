848bb58c0713253cd600f5f31f834c604c86b779
Simon Köck <54739715+simonkoeck@users.noreply.github.com>
2022-08-24 09:27:22 +0200

    add nodemailer

---
+++ cms/config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'simon@koeck.dev',
+        defaultReplyTo: 'simon@koeck.dev',
+      },
+    },
+  },
+  // ...
+});
